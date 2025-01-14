04af78a034838e161b5c816e75c9b7a96f7fb5ba
CarlosVazPorto <carlosvazporto@gmail.com>
2025-01-14 18:28:41 -0300

    Minhas tabelas, atualização e complementação

--- Alura/SQL/consultas_sql/Aula 01 - preparando o ambiente/minhas_consultas/criando_tabelas.sql
+++ Alura/SQL/consultas_sql/Aula 01 - preparando o ambiente/minhas_consultas/criando_tabelas.sql
@@ -37,3 +37,29 @@ CREATE TABLE fornecedores (
   cep VARCHAR(8) NOT NULL
 );
 
+CREATE TABLE clientes (
+  id TEXT NOT NULL,
+  nome VARCHAR(255),
+  telefone VARCHAR(20),
+  email VARCHAR(100) DEFAULT 'Sem e-mail',
+  endereco VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE pedidos (
+  id TEXT Primary KEY,
+  id_cliente TEXT,
+  data_hora DATETIME,
+  status VARCHAR(50),
+  Foreign Key (id_cliente) REFERENCES clientes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE itens_pedidos (
+  id_pedido TEXT,
+  id_produto TEXT,
+  quantidade INTEGER,
+  preco_unitario DECIMAL(10,2),
+  PRIMARY KEY (id_pedido, id_produto),
+  FOREIGN KEY (id_pedido) REFERENCES pedidos(id) ON DELETE CASCADE,
+  FOREIGN KEY (id_produto) REFERENCES produtos(id) ON DELETE CASCADE
+);
