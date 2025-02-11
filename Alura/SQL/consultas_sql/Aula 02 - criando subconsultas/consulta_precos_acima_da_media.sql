80bf783d3a467fd6952eec7b4141d5787b87ec36
CarlosVazPorto <carlosvazporto@gmail.com>
2025-02-11 18:24:30 -0300

    Acrescentado texto nos comentários.

--- Alura/SQL/consultas_sql/Aula 02 - criando subconsultas/consulta_precos_acima_da_media.sql
+++ Alura/SQL/consultas_sql/Aula 02 - criando subconsultas/consulta_precos_acima_da_media.sql
@@ -7,7 +7,7 @@ SELECT AVG(preco)
 FROM produtos;
 
 -- Consulta quais produtos têm seu preço acima da média
--- Observação: WHERE filtra diretamente linhas separadas de uma tabela 
+-- Observação: WHERE filtra dados não agrupados por GROUP BY
 -- Observação: HAVING filtra valores que já foram agrupados
 SELECT nome, preco 
 FROM produtos 
@@ -17,3 +17,9 @@ HAVING preco > (
   FROM produtos
   )
 ORDER BY preco DESC;
+
+-- O WHERE é usado para filtrar registros (linhas) com base em condições específicas que você define. Ele é aplicado antes que os dados sejam agrupados pela cláusula GROUP BY. Isso significa que você pode usar WHERE para filtrar as linhas individuais de dados antes de qualquer outra operação.
+-- Use WHERE quando precisar de uma condição para filtrar linhas antes de qualquer agrupamento.
+
+-- Em contrapartida, o HAVING é usado para filtrar dados depois que eles foram agrupados com a cláusula GROUP BY. Isso é útil quando você quer aplicar uma condição de filtro não nas linhas individuais, mas nos grupos resultantes. HAVING é frequentemente usado com funções de agregação, como COUNT(), SUM(), AVG(), etc.
+-- Use HAVING quando precisar aplicar um filtro depois de agrupar as linhas, especialmente quando estiver usando funções de agregação.
