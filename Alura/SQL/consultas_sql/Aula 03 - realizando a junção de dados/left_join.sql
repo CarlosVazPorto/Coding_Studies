45f38ff79471e2c176b9a3b21f793b99b29f733f
CarlosVazPorto <carlosvazporto@gmail.com>
2025-02-16 17:22:09 -0300

    Finalizando o arquivo left_join.sql

--- Alura/SQL/consultas_sql/Aula 03 - realizando a junção de dados/left_join.sql
+++ Alura/SQL/consultas_sql/Aula 03 - realizando a junção de dados/left_join.sql
@@ -7,4 +7,33 @@ INSERT INTO Clientes (id, nome, telefone, email, endereco)
 VALUES (28, 'João Santos', '215555678', 'joao.santos@email.com', 'Avenida Principal, 456, Cidade B'),
        (29, 'Carla Ferreira', '315557890', 'carla.ferreira@email.com', 'Travessa das Ruas, 789, Cidade C');
 
+-- Consulta inicial
+SELECT * 
+FROM clientes;
+
+-- Consulta que retorna todos os clientes e seus pedidos (existentes ou não)
+-- Clientes sem pedidos terão NULL como retorno 
+SELECT c.nome, p.id 
+FROM clientes AS c 
+LEFT JOIN pedidos AS p 
+ON c.id = p.id_cliente;
+
+-- Consulta que retorna os clientes que fizeram pedidos em um determinado mês (outubro)
+SELECT c.nome, p.id 
+FROM clientes AS c 
+LEFT JOIN pedidos AS p 
+ON c.id = p.id_cliente 
+WHERE strftime('%m', p.data_hora) = '10';
+
+-- Consulta que retorna os clientes que não fizeram pedidos em um determinado mês (outubro) 
+SELECT c.nome, x.id 
+FROM clientes AS c 
+LEFT JOIN 
+	(
+	SELECT p.id, p.id_cliente
+	FROM pedidos AS p 
+	WHERE strftime('%m', p.data_hora) = '10'
+	) AS x 
+ON c.id = x.id_cliente 
+WHERE x.id_cliente IS NULL;
 
