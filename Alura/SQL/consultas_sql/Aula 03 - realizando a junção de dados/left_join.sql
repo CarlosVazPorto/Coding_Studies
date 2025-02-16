d65704df66ee0d0664485274043c5ef9d9c9b2f4
CarlosVazPorto <carlosvazporto@gmail.com>
2025-02-16 16:53:11 -0300

    Adicionando left_join.sql e correções em union_e_union_all.sql

---
+++ Alura/SQL/consultas_sql/Aula 03 - realizando a junção de dados/left_join.sql
@@ -0,0 +1,10 @@
+-- Consultando dados nas tabelas do curso "Realizando consultas com SQL: Joins, Views e transações" da Alura
+-- Professora Danielle Oliveira
+-- Fevereiro/2025
+
+-- Inserindo novos clientes
+INSERT INTO Clientes (id, nome, telefone, email, endereco)
+VALUES (28, 'João Santos', '215555678', 'joao.santos@email.com', 'Avenida Principal, 456, Cidade B'),
+       (29, 'Carla Ferreira', '315557890', 'carla.ferreira@email.com', 'Travessa das Ruas, 789, Cidade C');
+
+
