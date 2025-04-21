ca0308b9cb70455370706b8c4498a3dfcc1a2735
CarlosVazPorto <carlosvazporto@gmail.com>
2025-04-21 18:30:13 -0300

    Exercise js_008 bird-watcher.js 1

---
+++ Exercism/JavaScript/js_008_bird-watcher.js
@@ -0,0 +1,20 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+    let total = 0;
+    for (let i = 0; i < birdsPerDay.length; i++) {
+      total += birdsPerDay[i];
+    }
+    return total;
+  }
+  
