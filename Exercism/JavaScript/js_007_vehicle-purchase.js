00ce246ee746f72e32541f8f8d204f4e126478e7
CarlosVazPorto <carlosvazporto@gmail.com>
2025-04-21 17:37:10 -0300

    Exercise j5_007 vehicle-purchase.js 1

---
+++ Exercism/JavaScript/js_007_vehicle-purchase.js
@@ -0,0 +1,20 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines whether or not you need a license to operate a certain kind of vehicle.
+ *
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
+ */
+export function needsLicense(kind) {
+    if (kind === 'car' || kind === 'truck') {
+      return true;  
+    } else {
+      return false;
+    }
+  }
+  
