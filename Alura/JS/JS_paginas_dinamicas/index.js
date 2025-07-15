c4073ab78aa034a22e541720595386ea8b4e9fd3
CarlosVazPorto <carlosvazporto@gmail.com>
2025-07-15 17:12:31 -0300

    Construção dos elementos da lista e do input tipo checkbox no index.js

--- Alura/JS/JS_paginas_dinamicas/index.js
+++ Alura/JS/JS_paginas_dinamicas/index.js
@@ -1,16 +1,26 @@
+// Variáveis de acesso aos dados da página
 const inputItem = document.getElementById("input-item");
-
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
+// Lógica do botão "Salvar item"
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
+    
+    // Prevenção da inserção de itens vazios na lista
     if (inputItem.value === "") {
         alert("Por favor, insira um item!");
         return;
     }
 
+    // Elementos da lista
     const itemDaLista = document.createElement("li");
     const containerItemdaLista = document.createElement("div");
     containerItemdaLista.classList.add("lista-item-container");
+    
+    // Input do tipo checkbox com id dinâmico
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
     
 })
