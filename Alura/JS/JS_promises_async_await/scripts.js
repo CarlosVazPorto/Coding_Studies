90cfc7ef38175ca6cac2ad43c16b2e8825dc79c5
CarlosVazPorto <carlosvazporto@gmail.com>
2025-07-22 13:37:04 -0300

    Estilização da 'image-projeto.png' e input do botão 'Carregar imagem'

--- Alura/JS/JS_promises_async_await/scripts.js
+++ Alura/JS/JS_promises_async_await/scripts.js
@@ -1,1 +1,8 @@
-let a = 1;
+// Acesso aos elementos da página para upload de imagens
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload");
+
+// Ouvinte de eventos do botão 'Carregar imagem'
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
