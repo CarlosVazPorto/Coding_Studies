0066b344a4e14ae7a31152c0bbf12be98215c5d7
CarlosVazPorto <carlosvazporto@gmail.com>
2025-07-27 17:13:22 -0300

    feat: app/metodoFilter.js versão finalizada

--- Alura/JS/JS_alura_books/app/metodoFilter.js
+++ Alura/JS/JS_alura_books/app/metodoFilter.js
@@ -1,10 +1,11 @@
 // Funcionalidade para exibir livros segundo a sua categoria
 
-const btnFiltrarLivrosDeFront = document.getElementById("btnFiltrarLivrosFront");
-
-btnFiltrarLivrosDeFront.addEventListener("click", filtrarLivros)
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach(btn => btn.addEventListener("click", filtrarLivros));
 
 function filtrarLivros() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == "front-end");
-    console.table(livrosFiltrados);
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
 }
