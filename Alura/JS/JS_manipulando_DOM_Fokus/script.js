f307e6261ef5122434e385d995ad31b239e6bd38
CarlosVazPorto <carlosvazporto@gmail.com>
2025-07-30 17:26:33 -0300

    feat: script.js Mudança da imagem ao acionar os botões

--- Alura/JS/JS_manipulando_DOM_Fokus/script.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script.js
@@ -1,16 +1,25 @@
+// Variáveis de manipulação do DOM
 const html = document.querySelector("html");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
 
+// Trocas de cor de fundo e imagem secundárias a evento nos botões
 focoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "foco");
+    banner.setAttribute("src", "/imagens/foco.png");
+    banner.setAttribute("alt", "Foco");
 })
 
 curtoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "descanso-curto");
+    banner.setAttribute("src", "/imagens/descanso-curto.png");
+    banner.setAttribute("alt", "Descanso curto");    
 })
 
 longoBt.addEventListener("click", () => {
     html.setAttribute("data-contexto", "descanso-longo");
+    banner.setAttribute("src", "/imagens/descanso-longo.png");
+    banner.setAttribute("alt", "Descanso longo");
 })
