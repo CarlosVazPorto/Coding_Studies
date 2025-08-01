52230610a6a42c2f9a94da11a884a4584b737d8c
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-01 17:50:20 -0300

    feat: script.js Início da implantação do temporizador

--- Alura/JS/JS_manipulando_DOM_Fokus/script.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script.js
@@ -6,9 +6,14 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musica = new Audio("/sons/luna-rise-part-one.mp3");
 musica.loop = true;
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 // Evento para ligar / desligar a música
 musicaFocoInput.addEventListener("change", () => {
@@ -69,4 +74,16 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    iniciar();
+    tempoDecorridoEmSegundos -= 1;
+    console.log("Temporizador: " + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener("click", contagemRegressiva);
+
+function iniciar() {
+    intervaloId = setInterval(contagemRegressiva, 1000);
 }
