fb826e6025e0399077e90f8e401318b3b0b8813e
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-02 15:51:14 -0300

    feat: script.js Ajustes finais do temporizador na tela

--- Alura/JS/JS_manipulando_DOM_Fokus/script.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script.js
@@ -34,21 +34,25 @@ musicaFocoInput.addEventListener("change", () => {
 
 // Alterações da página segundo o contexto
 focoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto("foco");
     focoBt.classList.add("active");
 })
 
 curtoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto("descanso-curto");
     curtoBt.classList.add("active");
 })
 
 longoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto("descanso-longo");
     longoBt.classList.add("active");
 })
 
 function alterarContexto(contexto) {
+    mostrarTempo();
     botoes.forEach((contexto => {
         contexto.classList.remove("active");
     }))
@@ -87,7 +91,7 @@ function alterarContexto(contexto) {
 // Manejo do tempo
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0){
-        //audioTempoFinalizado.play();
+        audioTempoFinalizado.play();
         alert("Tempo finalizado!");
         zerar();
         return;
@@ -118,8 +122,14 @@ function zerar() {
 }
 
 function mostrarTempo() {
-    const tempo = tempoDecorridoEmSegundos;
-    tempoNaTela.innerHTML = `${tempo}`;
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString(
+        "pt-br", 
+        {
+        minute: "2-digit", 
+        second: "2-digit"
+        });
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
 }
 
 mostrarTempo();
