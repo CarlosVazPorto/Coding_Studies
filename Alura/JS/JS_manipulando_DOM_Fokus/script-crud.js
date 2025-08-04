b28d85e2e1f1a9387d6ec4b68226f181bc37e494
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-04 18:23:39 -0300

    feat: script-crud.js API JSON.stringify

--- Alura/JS/JS_manipulando_DOM_Fokus/script-crud.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script-crud.js
@@ -18,5 +18,5 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
     }
 
     tarefas.push(tarefa);
-    localStorage.setItem("tarefas", tarefas);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
 })
