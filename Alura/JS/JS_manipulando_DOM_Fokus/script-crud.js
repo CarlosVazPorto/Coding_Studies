a51a1d2a307ee494c43bfc309c369d26ef403385
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-10 19:03:05 -0300

    feat: script-crud.js Remoção de tarefas concluídas

--- Alura/JS/JS_manipulando_DOM_Fokus/script-crud.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script-crud.js
@@ -7,7 +7,10 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 const btnCancelarTarefa = document.querySelector(".app__form-footer__button--cancel");
 const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
-const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+// const btnRemoverTodas = document.querySelector("#btn-remover-todas");
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
@@ -123,3 +126,12 @@ document.addEventListener("FocoFinalizado", () => {
         atualizarTarefas();
     }
 })
+
+btnRemoverConcluidas.onclick = () => {
+    const seletor = ".app__section-task-list-item-complete";
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove();
+    })
+    tarefas = tarefas.filter(tarefa => !tarefa.completa);
+    atualizarTarefas();
+}
