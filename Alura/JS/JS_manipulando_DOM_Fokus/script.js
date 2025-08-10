5f8a90ef779e09a0d5eaab2c991719f0e48eb0ba
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-10 19:27:23 -0300

    fix: script.js Voltando timer do Foco para 1500s

--- Alura/JS/JS_manipulando_DOM_Fokus/script.js
+++ Alura/JS/JS_manipulando_DOM_Fokus/script.js
@@ -20,7 +20,7 @@ const audioPlay = new Audio("/sons/play.wav");
 const audioPausa = new Audio("/sons/pause.mp3");
 
 // Variáveis de contagem de tempo
-let tempoDecorridoEmSegundos = 5; //1500;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 // Evento para ligar / desligar a música
