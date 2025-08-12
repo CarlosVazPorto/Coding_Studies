ddeab157a78b0a6ffc9c237411cf33f7b47e458e
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-12 11:52:56 -0300

    feat: app.js while

--- Alura/JS/JS_logica_de_programacao/app.js
+++ Alura/JS/JS_logica_de_programacao/app.js
@@ -1,18 +1,22 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 8;
+let numeroSecreto = 5;
 // console.log('Número secreto: ' + numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 // console.log('Valor do chute: ' + chute);
-
 // console.log('Resultado da comparação: ', chute == numeroSecreto);
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor do que ${chute}`);
+// Enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é maior do que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor do que ${chute}`);
+        } else {
+            alert(`O número secreto é maior do que ${chute}`);
+        }
     }
 }
