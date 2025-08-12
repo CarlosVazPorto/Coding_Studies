fc3f5403f5dde50c4aa2a4c9cd391f8a5f76b1c9
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-12 10:49:31 -0300

    feat: app.js console.log()

--- Alura/JS/JS_logica_de_programacao/app.js
+++ Alura/JS/JS_logica_de_programacao/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 8;
-console.log(numeroSecreto);
+// console.log('Número secreto: ' + numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
+// console.log('Valor do chute: ' + chute);
+
+// console.log('Resultado da comparação: ', chute == numeroSecreto);
 
 // Se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
