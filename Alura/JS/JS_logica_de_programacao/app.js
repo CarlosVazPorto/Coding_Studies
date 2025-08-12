b615a12b6538d22b8bc58d82e17063e37f10218a
CarlosVazPorto <carlosvazporto@gmail.com>
2025-08-12 11:42:35 -0300

    feat: app.js condicionais se o palpite estiver errado

--- Alura/JS/JS_logica_de_programacao/app.js
+++ Alura/JS/JS_logica_de_programacao/app.js
@@ -10,5 +10,9 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(');
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor do que ${chute}`);
+    } else {
+        alert(`O número secreto é maior do que ${chute}`);
+    }
 }
