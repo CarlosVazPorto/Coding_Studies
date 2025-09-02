152dd5254b1cb4275b87820689f4952c2f544bc9
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-02 11:23:22 -0300

    fix(script.js): MoniBank - objeto mensagens

--- Alura/JS/JS_monibank/js/script.js
+++ Alura/JS/JS_monibank/js/script.js
@@ -36,13 +36,13 @@ const mensagens = {
     rg: {
         valueMissing: "O campo de RG não pode estar vazio.",
         patternMismatch: "Por favor, preencha um RG válido.",
-        tooShort: "O campo de RG não tem caractéres suficientes."
+        tooShort: "O campo de RG não tem caracteres suficientes."
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
         patternMismatch: "Por favor, preencha um CPF válido.",
         customError: "O CPF digitado não existe.",
-        tooShort: "O campo de CPF não tem caractéres suficientes."
+        tooShort: "O campo de CPF não tem caracteres suficientes."
     },
     aniversario: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
