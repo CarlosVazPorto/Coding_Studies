aa568dfa925291073428b7904d2df89e9e9221a7
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-02 13:39:20 -0300

    feat(script.js): MoniBank - inserção das mensagens de erro na tela

--- Alura/JS/JS_monibank/js/script.js
+++ Alura/JS/JS_monibank/js/script.js
@@ -54,6 +54,8 @@ const mensagens = {
 }
 
 function verificaCampo(campo) {
+    let mensagem = "";
+
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
     }
@@ -62,7 +64,20 @@ function verificaCampo(campo) {
         ehMaiorDeIdade(campo);
     }
 
-    console.log(campo.validity);
+    tiposDeErros.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    })
+
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
+
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
 
 }
-
