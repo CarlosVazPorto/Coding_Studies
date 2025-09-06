5becdf034b949e40a50e65cf969a6c0b079962ef
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-06 11:30:54 -0300

    feat(ui.js): renderização do DOM da lista de pensamentos

--- Alura/JS/JS_memoteca_crud/js/api.js
+++ Alura/JS/JS_memoteca_crud/js/api.js
@@ -3,7 +3,8 @@ const api = {
         try {
             const response = await fetch('http://localhost:3000/pensamentos');
             return await response.json();
-        } catch (error) {
+        } 
+        catch (error) {
             alert('Erro ao buscar pensamentos');
             throw error;
         }
