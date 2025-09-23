ddf0317187b69c7689b97949897fa24897babbfb
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-23 16:48:00 -0300

    feat(main.js): validarData()

--- Alura/JS/JS_memoteca_crud/js/main.js
+++ Alura/JS/JS_memoteca_crud/js/main.js
@@ -52,3 +52,11 @@ async function manipularBusca() {
     }
 
 }
+
+function validarData(data) {
+    const dataAtual = new Date();
+    const dataInserida = new Date(data);
+
+    return dataInserida <= dataAtual;
+
+}
