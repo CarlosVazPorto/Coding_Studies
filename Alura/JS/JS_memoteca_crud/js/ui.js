12d6fd6bf54370fd77c222c7d439ff377fd5eb25
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-23 11:57:01 -0300

    feat(ui.js): renderização das mudanças no status de favorito

--- Alura/JS/JS_memoteca_crud/js/ui.js
+++ Alura/JS/JS_memoteca_crud/js/ui.js
@@ -57,9 +57,20 @@ const ui = {
 
         const botaoFavorito = document.createElement("button");
         botaoFavorito.classList.add("botao-favorito");
+        botaoFavorito.onclick = async () => {
+            try {
+                await api.atualizarFavorito(pensamento.id, !pensamento.favorito);
+                ui.renderizarPensamentos();
+            } catch (error) {
+                alert("Erro ao atualizar status de favorito do pensamento.");
+                throw error;
+            }
+        }
 
         const iconeFavorito = document.createElement("img");
-        iconeFavorito.src = "assets/imagens/icone-favorito_outline.png";
+        iconeFavorito.src = pensamento.favorito ? 
+            "assets/imagens/icone-favorito.png" :
+            "assets/imagens/icone-favorito_outline.png";
         iconeFavorito.alt = "Ícone de favorito";
 
         const botaoEditar = document.createElement("button");
