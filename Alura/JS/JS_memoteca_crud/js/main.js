2962d0cf81c0d96cbb9e61f6d1016d77f3655c8c
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-23 16:42:55 -0300

    feat(main.js): inclusão da data na submissão do formulário

--- Alura/JS/JS_memoteca_crud/js/main.js
+++ Alura/JS/JS_memoteca_crud/js/main.js
@@ -7,25 +7,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const formularioPensamento = document.getElementById("pensamento-form");
     const botaoCancelar = document.getElementById("botao-cancelar");
     const inputBusca = document.getElementById("campo-busca");
-
+    
     formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
     botaoCancelar.addEventListener("click", manipularCancelamento);
     inputBusca.addEventListener("input", manipularBusca);
-
+    
 })
 
 async function manipularSubmissaoFormulario(event) {
     event.preventDefault();
-
+    
     const id = document.getElementById("pensamento-id").value;
     const conteudo = document.getElementById("pensamento-conteudo").value;
     const autoria = document.getElementById("pensamento-autoria").value;
+    const data = document.getElementById("pensamento-data").value;
 
     try {
         if (id) {
-            await api.editarPensamento({ id, conteudo, autoria });
+            await api.editarPensamento({ id, conteudo, autoria, data });
         } else {
-            await api.salvarPensamento({ conteudo, autoria });
+            await api.salvarPensamento({ conteudo, autoria, data });
         }
         ui.renderizarPensamentos();
     }
