b64b3db765c39e6840e734d312dc91614b6a8887
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-24 18:48:09 -0300

    feat(JS_lista_de_filmes/main.js): validarData()

--- Alura/JS/JS_lista_de_filmes/js/main.js
+++ Alura/JS/JS_lista_de_filmes/js/main.js
@@ -48,3 +48,9 @@ async function manipularBusca() {
     throw error;
   }
 }
+
+function validarData(data) {
+  const dataAtual = new Date();
+  const dataInserida = new Date(data);
+  return dataInserida <= dataAtual;
+}
