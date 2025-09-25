ad01105be4840936eff86a43e9b493fb8e574cb7
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-25 15:46:53 -0300

    feat(api.js): converterStringParaData()

--- Alura/JS/JS_memoteca_crud/js/api.js
+++ Alura/JS/JS_memoteca_crud/js/api.js
@@ -1,5 +1,10 @@
 const URL_BASE = "http://localhost:3000";
 
+const converterStringParaData = (dataString) => {
+    const [ano, mes, dia] = dataString.split("-");
+    return new Date(Date.UTC(ano, mes, dia));
+}
+
 const api = {
     async buscarPensamentos() {
         try {
