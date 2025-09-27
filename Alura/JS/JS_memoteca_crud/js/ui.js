2f2018f4e6bde2a54fa632aed5d5110cc0947341
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-27 11:29:05 -0300

    feat(ui.js): apresentação da data no card

--- Alura/JS/JS_memoteca_crud/js/ui.js
+++ Alura/JS/JS_memoteca_crud/js/ui.js
@@ -7,6 +7,7 @@ const ui = {
         document.getElementById("pensamento-id").value = pensamento.id;
         document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
         document.getElementById("pensamento-autoria").value = pensamento.autoria;
+        document.getElementById("pensamento-data").value = pensamento.data;
     },
     
     async renderizarPensamentos(pensamentosFiltrados = null) {
@@ -54,6 +55,10 @@ const ui = {
         const pensamentoAutoria = document.createElement("div");
         pensamentoAutoria.textContent = pensamento.autoria;
         pensamentoAutoria.classList.add("pensamento-autoria");
+
+        const pensamentoData = document.createElement("div");
+        pensamentoData.textContent = pensamento.data;
+        pensamentoData.classList.add("pensamento-data");
 
         const botaoFavorito = document.createElement("button");
         botaoFavorito.classList.add("botao-favorito");
@@ -112,6 +117,7 @@ const ui = {
         li.appendChild(iconeAspas);
         li.appendChild(pensamentoConteudo);
         li.appendChild(pensamentoAutoria);
+        li.appendChild(pensamentoData);
         li.appendChild(icones);
         
         listaPensamentos.appendChild(li);
