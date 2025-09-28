23ca2f00b2adf14dc42800942df045753181f4b2
CarlosVazPorto <carlosvazporto@gmail.com>
2025-09-28 11:53:34 -0300

    feat(main.js): regex aprimorada da validação de autoria

--- Alura/JS/JS_memoteca_crud/js/main.js
+++ Alura/JS/JS_memoteca_crud/js/main.js
@@ -2,6 +2,7 @@ import ui from "./ui.js"
 import api from "./api.js"
 
 const regexConteudo = /^[\p{L}0-9\s\p{P}]{3,}$/u;
+const regexAutoria = /^[\p{L}\s]{2,20}$/;
 
 function validarConteudo(conteudo) {
     return regexConteudo.test(conteudo);
