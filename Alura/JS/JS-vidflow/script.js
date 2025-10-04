a6721cbb12b104939464d0e09c9e2863b58a70c0
CarlosVazPorto <carlosvazporto@gmail.com>
2025-10-04 18:34:33 -0300

    feat(vidflow/script.js): try / catch em buscarEMostrarVideos()

--- Alura/JS/JS-vidflow/script.js
+++ Alura/JS/JS-vidflow/script.js
@@ -2,22 +2,26 @@ const urlEndpointVideos = "http://localhost:3000/videos";
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
-    const busca = await fetch(urlEndpointVideos);
-    const videos = await busca.json();
-        
-    videos.forEach((video) => {
-        containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe src="${video.url}" title="${video.titulo}" frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
-                </div>
-            </li>
-        `;
-    });    
+    try {
+        const busca = await fetch(urlEndpointVideos);
+        const videos = await busca.json();
+            
+        videos.forEach((video) => {
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+            `;
+        });
+    } catch (error) {
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
 };
 
 buscarEMostrarVideos();
