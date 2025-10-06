fd27e566be0f67ebbfe70355e338f40e850c8423
CarlosVazPorto <carlosvazporto@gmail.com>
2025-10-06 14:31:46 -0300

    feat(vidflow/script.js): refatorção da lógica de pesquisa em filtrarPesquisa()

--- Alura/JS/JS-vidflow/script.js
+++ Alura/JS/JS-vidflow/script.js
@@ -35,19 +35,11 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
 function filtrarPesquisa() {
     const videos = document.querySelectorAll(".videos__item");
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
 
-    if (barraDePesquisa.value != "") {
-        for (let video of videos) {
-            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
-            let valorFiltro = barraDePesquisa.value.toLowerCase();
-            
-            if (!titulo.includes(valorFiltro)) {
-                video.style.display = "none";
-            } else {
-                video.style.display = "block";
-            };
-        };
-    } else {
-        video.style.display = "block";
-    };
+    videos.forEach((video) => {
+        const titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? "block" : "none" : "block";
+    })
 }
