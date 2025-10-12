a4e0b0d2aaac82af53296bb323f1d541cfe28a04
CarlosVazPorto <carlosvazporto@gmail.com>
2025-10-12 16:29:18 -0300

    feat(aluraplay/conectaApi.js): function listaVideos()' aprimorada e export const conectaApi

--- Alura/JS/JS_aluraplay/js/conectaApi.js
+++ Alura/JS/JS_aluraplay/js/conectaApi.js
@@ -2,7 +2,11 @@ const urlVideos = "http://localhost:3000/videos";
 
 async function listaVideos() {
   const conexao = await fetch(urlVideos);
-  console.log(conexao);
+  const conexaoConvertida = await conexao.json();
+
+  return conexaoConvertida;
 }
 
-listaVideos();
+export const conectaApi = {
+  listaVideos,
+};
