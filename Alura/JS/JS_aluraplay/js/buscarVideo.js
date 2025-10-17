bbc6be47cbcc56baba81239f28b3c1378b157f5d
CarlosVazPorto <carlosvazporto@gmail.com>
2025-10-17 19:16:25 -0300

    feat(aluraplay/buscarVideo.js): aprimorando buscarVideo()

--- Alura/JS/JS_aluraplay/js/buscarVideo.js
+++ Alura/JS/JS_aluraplay/js/buscarVideo.js
@@ -7,7 +7,18 @@ async function buscarVideo(evento) {
   const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
   const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-  console.log(busca);
+  const lista = document.querySelector("[data-lista]");
+
+  busca.forEach((elemento) =>
+    lista.appendChild(
+      constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem
+      )
+    )
+  );
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
