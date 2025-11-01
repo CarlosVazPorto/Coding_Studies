52cb185048dbdbc908b3422bb603dd436cda929b
CarlosVazPorto <carlosvazporto@gmail.com>
2025-11-01 16:11:23 -0300

    feat(codeconnect/componentes/Filtro/index.jsx): Filtro()

--- Alura/React/codeconnect/src/App.jsx
+++ Alura/React/codeconnect/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import BarraDePesquisa from './componentes/BarraDePesquisa';
-import Sidebar from './componentes/SIdebar';
+import Sidebar from './componentes/Sidebar';
+import Filtro from './componentes/Filtro';
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
       <BarraDePesquisa />
+      <Filtro />
     </div>
   );
 };
