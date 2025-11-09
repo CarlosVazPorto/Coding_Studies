8c48c96a391098c632b1d27f3e09a4e48e89c4fb
CarlosVazPorto <carlosvazporto@gmail.com>
2025-11-09 16:49:19 -0300

    feat(organo/src/App.jsx): edição teste do texto

--- Alura/React/organo/src/App.jsx
+++ Alura/React/organo/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          A Mônica é a minha patinha!
         </p>
       </div>
       <p className="read-the-docs">
