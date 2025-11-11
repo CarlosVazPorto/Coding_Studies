8c56c5a8b1b660af7425cdf040370cb8016cf6c6
CarlosVazPorto <carlosvazporto@gmail.com>
2025-11-11 18:06:45 -0300

    feat(organo/src/componentes/Formulario): onSubmit

--- Alura/React/organo/src/componentes/Formulario/index.jsx
+++ Alura/React/organo/src/componentes/Formulario/index.jsx
@@ -15,9 +15,14 @@ const Formulario = () => {
         'Inovação e Gestão'
     ];
     
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log("O formulário foi enviado!");
+    };
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto
                     label="Nome" 
