f7a4495504865625cea51a575145b79b8f70a723
CarlosVazPorto <carlosvazporto@gmail.com>
2025-11-13 17:48:42 -0300

    (organo/src/componentes/CampoTexto/index.jsx): captando o valor do input com onChange

--- Alura/React/organo/src/componentes/CampoTexto/index.jsx
+++ Alura/React/organo/src/componentes/CampoTexto/index.jsx
@@ -1,12 +1,21 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+    
+    let valorDigitado = "";
+
+    const aoDigitar = (evento) => {
+        valorDigitado = evento.target.value;
+        console.log(valorDigitado);
+    };
+    
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
             <input 
+                onChange={aoDigitar}
                 required={props.required}
                 placeholder={props.placeholder}
             />
