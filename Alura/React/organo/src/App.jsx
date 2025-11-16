1d7ef5f764d32048bab1eb35dd94f6305da56ead
CarlosVazPorto <carlosvazporto@gmail.com>
2025-11-16 16:05:34 -0300

    (organo/src/App.jsx): array dos times

--- Alura/React/organo/src/App.jsx
+++ Alura/React/organo/src/App.jsx
@@ -5,6 +5,44 @@ import Time from './componentes/Team';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corFundos: 'var(--programacao-fundos)',
+      corDestaques: 'var(--programacao-destaques)'
+    },
+    {
+      nome: 'Front-End',
+      corFundos: 'var(--frontend-fundos)',
+      corDestaques: 'var(--frontend-destaques)'
+    },
+    {
+      nome: 'Data Science',
+      corFundos: 'var(--datascience-fundos)',
+      corDestaques: 'var(--datascience-destaques)'
+    },
+    {
+      nome: 'Devops',
+      corFundos: 'var(--devops-fundos)',
+      corDestaques: 'var(--devops-destaques)'
+    },
+    {
+      nome: 'UX e Design',
+      corFundos: 'var(--ux-fundos)',
+      corDestaques: 'var(--ux-destaques)'
+    },
+    {
+      nome: 'Mobile',
+      corFundos: 'var(--mobile-fundos)',
+      corDestaques: 'var(--mobile-destaques)'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corFundos: 'var(--inovacao-fundos)',
+      corDestaques: 'var(--inovacao-destaques)'
+    },
+  ]
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoAdicionarNovoColaborador = (colaborador) => {
